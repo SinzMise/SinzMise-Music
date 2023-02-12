44f031887b382835a9dffaca3a9082ebc5998295
SinzMise <mail@sinzmise.cf>
2023-02-12 20:43:13 +0800

    涓婁紶闊充箰

--- aplayer.js
+++ aplayer.js
@@ -4,26 +4,26 @@ const ap = new APlayer({
         {
             name: 'Eutopia',
             artist: 'Yoohsic Roomz',
-            url: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/musics/1.mp3',
-            cover: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/images/3.jpg',
+            url: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/musics/1.mp3',
+            cover: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/images/3.jpg',
         },
         {
             name: 'Town of Windmill',
             artist: 'a_hisa',
-            url: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/musics/2.mp3',
-            cover: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/images/2.jpg',
+            url: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/musics/2.mp3',
+            cover: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/images/2.jpg',
         },
         {
             name: 'Hiro',
             artist: 'Sight of Wonders',
-            url: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/musics/3.mp3',
-            cover: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/images/3.jpg',
+            url: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/musics/3.mp3',
+            cover: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/images/3.jpg',
         },
         {
             name: 'Komorebi',
             artist: 'm-taku',
-            url: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/musics/4.mp3',
-            cover: 'https://cdn.staticaly.com/gh/SinzMise/SinzMise-Music/images/4.jpg',
+            url: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/musics/4.mp3',
+            cover: 'https://cdn1.tianli0.top/gh/SinzMise/SinzMise-Music@main/images/4.jpg',
         },
     ]
 });
